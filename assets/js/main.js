2e987d31e64c0c523e425a0e5c9c8f72f48cc028
StephanPagel <stephan.pagel84@gmail.com>
2022-05-26 23:58:22 +0200

    finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,19 +8,6 @@ let inputChoiceRock = document.getElementById('inputChoiceRock');
 let inputChoicePaper = document.getElementById('inputChoicePaper');
 let inputChoiceScissors = document.getElementById('inputChoiceScissors');
 
-// document.querySelector('#inputChoiceRock').addEventListener('click', function () {
-//     usersChoice = 'rock';
-//     console.log('users choice:', usersChoice);
-// })
-// document.querySelector('#inputChoicePaper').addEventListener('click', function () {
-//     usersChoice = 'paper';
-//     console.log('users choice:', usersChoice);
-// })
-// document.querySelector('#inputChoiceScissors').addEventListener('click', function () {
-//     usersChoice = 'scissors';
-//     console.log('users choice:', usersChoice);
-// })
-
 // Output
 
 let outputRoundsCurrent = document.getElementById('outputRoundsCurrent');
@@ -30,8 +17,6 @@ let outputScoreComputer = document.getElementById('outputScoreComputer');
 let outputResult = document.getElementById('outputResult');
 let outputInstruction = document.getElementById('outputInstruction');
 
-
-
 let rounds;
 let usersChoice;
 let computersChoice;
@@ -39,7 +24,7 @@ let scoreUser = 0;
 let scoreComputer = 0;
 let countRounds = 0;
 
-// Testing
+// Set variables
 
 outputRoundsCurrent.innerHTML = 0;
 outputRoundsTotal.innerHTML = 0;
@@ -47,18 +32,6 @@ outputScoreUser.innerHTML = 0;
 outputScoreComputer.innerHTML = 0;
 outputResult.innerHTML = "Let's go";
 outputInstruction.innerHTML = "Please select a number of rounds first."
-
-// Set variables
-
-// inputChoiceRock.addEventListener("click", () => {
-//     inputChoiceRock.clicked = true;
-// });
-// inputChoicePaper.addEventListener("click", () => {
-//     inputChoicePaper.clicked = true;
-// });
-// inputChoiceScissors.addEventListener("click", () => {
-//     inputChoiceScissors.clicked = true;
-// });
 
 // Select rounds
 
@@ -68,32 +41,28 @@ selectRounds = () => {
         outputRoundsTotal.innerHTML = rounds;
         outputInstruction.style.color = 'black';
         outputInstruction.innerHTML = 'Make a move.'
-        // console.log(rounds);
     }
     else if (inputRounds10.checked) {
         rounds = 10;
         outputRoundsTotal.innerHTML = rounds;
         outputInstruction.style.color = 'black';
         outputInstruction.innerHTML = 'Make a move.'
-        // console.log(rounds);
     }
     else if (inputRounds15.checked) {
         rounds = 15;
         outputRoundsTotal.innerHTML = rounds;
         outputInstruction.style.color = 'black';
         outputInstruction.innerHTML = 'Make a move.'
-        // console.log(rounds);
     }
     else if (inputRounds20.checked) {
         rounds = 20;
         outputRoundsTotal.innerHTML = rounds;
         outputInstruction.style.color = 'black';
         outputInstruction.innerHTML = 'Make a move.'
-        // console.log(rounds);
     }
 }
 
-// getUsersChoice = () => {}
+// Computers Choice
 
 getComputersChoice = () => {
     computersChoice = Math.floor(Math.random() * 3) + 1;
@@ -108,114 +77,93 @@ getComputersChoice = () => {
             computersChoice = 'scissors'
             break;
     }
-    // console.log(computersChoice);
 }
 
-compareChoices = () => {
-    // usersChoice = 'rock';
-    // let x = 0;
-    // while (x <= rounds) {
-    //     x++;
+// Who wins?
 
+compareChoices = () => {
     switch (true) {
         case (usersChoice === 'rock' && computersChoice === 'rock'):
             countRounds++;
             outputRoundsCurrent.innerHTML = countRounds;
             outputResult.innerHTML = `It's a draw! You both chose ${(usersChoice.toUpperCase())}.`;
+            outputResult.style.color = 'black';
             break;
         case (usersChoice === 'rock' && computersChoice === 'paper'):
-            // console.log('user: ', usersChoice);
-            // console.log('computer: ', computersChoice);
-            console.log('COMPUTER WINS');
             countRounds++;
             outputRoundsCurrent.innerHTML = countRounds;
             scoreComputer++;
             outputScoreComputer.innerHTML = scoreComputer;
             outputResult.innerHTML = `You lose! ${(usersChoice.toUpperCase())} is defeated by ${(computersChoice.toUpperCase())}.`
-            // console.log(outputRoundsCurrent.value);
+            outputResult.style.color = 'red';
             break;
         case (usersChoice === 'rock' && computersChoice === 'scissors'):
-            // console.log('user: ', usersChoice);
-            // console.log('computer: ', computersChoice);
-            console.log('YOU WIN');
             countRounds++;
             outputRoundsCurrent.innerHTML = countRounds;
             scoreUser++;
             outputScoreUser.innerHTML = scoreUser;
             outputResult.innerHTML = `You win! ${(usersChoice.toUpperCase())} beats ${(computersChoice.toUpperCase())}.`
+            outputResult.style.color = 'green';
             break;
         case (usersChoice === 'paper' && computersChoice === 'rock'):
-            // console.log('user: ', usersChoice);
-            // console.log('computer: ', computersChoice);
-            console.log('YOU WIN');
             countRounds++;
             outputRoundsCurrent.innerHTML = countRounds;
             scoreUser++;
             outputScoreUser.innerHTML = scoreUser;
             outputResult.innerHTML = `You win! ${(usersChoice.toUpperCase())} beats ${(computersChoice.toUpperCase())}.`
+            outputResult.style.color = 'green';
             break;
         case (usersChoice === 'paper' && computersChoice === 'paper'):
-            // console.log('user: ', usersChoice);
-            // console.log('computer: ', computersChoice);
-            console.log('TIE');
             countRounds++;
             outputRoundsCurrent.innerHTML = countRounds;
             outputResult.innerHTML = `It's a draw! You both chose ${(usersChoice.toUpperCase())}.`;
+            outputResult.style.color = 'black';
             break;
         case (usersChoice === 'paper' && computersChoice === 'scissors'):
-            // console.log('user: ', usersChoice);
-            // console.log('computer: ', computersChoice);
-            console.log('COMPUTER WINS');
             countRounds++;
             outputRoundsCurrent.innerHTML = countRounds;
             scoreComputer++;
             outputScoreComputer.innerHTML = scoreComputer;
             outputResult.innerHTML = `You lose! ${(usersChoice.toUpperCase())} is defeated by ${(computersChoice.toUpperCase())}.`
+            outputResult.style.color = 'red';
             break;
         case (usersChoice === 'scissors' && computersChoice === 'rock'):
-            // console.log('user: ', usersChoice);
-            // console.log('computer: ', computersChoice);
-            console.log('COMPUTER WINS');
             countRounds++;
             outputRoundsCurrent.innerHTML = countRounds;
             scoreComputer++;
             outputScoreComputer.innerHTML = scoreComputer;
             outputResult.innerHTML = `You lose! ${(usersChoice.toUpperCase())} is defeated by ${(computersChoice.toUpperCase())}.`
+            outputResult.style.color = 'red';
             break;
         case (usersChoice === 'scissors' && computersChoice === 'paper'):
-            // console.log('user: ', usersChoice);
-            // console.log('computer: ', computersChoice);
-            console.log('YOU WIN');
             countRounds++;
             outputRoundsCurrent.innerHTML = countRounds;
             scoreUser++;
             outputScoreUser.innerHTML = scoreUser;
             outputResult.innerHTML = `You win! ${(usersChoice.toUpperCase())} beats ${(computersChoice.toUpperCase())}.`
+            outputResult.style.color = 'green';
             break;
         case (usersChoice === 'scissors' && computersChoice === 'scissors'):
-            // console.log('user: ', usersChoice);
-            // console.log('computer: ', computersChoice);
-            console.log('TIE');
             countRounds++;
             outputRoundsCurrent.innerHTML = countRounds;
             outputResult.innerHTML = `It's a draw! You both chose ${(usersChoice.toUpperCase())}.`;
+            outputResult.style.color = 'black';
             break;
     }
-    // if (x === rounds) {
-    //     console.log('FINISHED', ' user: ', usersScore, ' computer: ', computersScore);
-    //     break;
-    // }
-
     if (countRounds === rounds) {
         if (scoreUser < scoreComputer) {
             outputResult.innerHTML = `You lose... You ${scoreUser} : ${scoreComputer} Computer`;
+            outputResult.style.color = 'red';
+            outputInstruction.innerHTML = 'Hit RESTART to play again.';
         } else if (scoreUser > scoreComputer) {
             outputResult.innerHTML = `You win! You ${scoreUser} : ${scoreComputer} Computer`;
+            outputResult.style.color = 'green';
+            outputInstruction.innerHTML = 'Hit RESTART to play again.';
         } else if (scoreUser === scoreComputer) {
-            outputResult.innerHTML = `It's a draw! ${scoreUser} each`;
+            outputResult.innerHTML = `It's a draw! ${scoreUser} Point(s)each`;
+            outputResult.style.color = 'black';
+            outputInstruction.innerHTML = 'Hit RESTART to play again.';
         }
-        console.log('FINISHED', ' user: ', scoreUser, ' computer: ', scoreComputer);
-        return;
     }
 }
 
@@ -225,7 +173,6 @@ chooseRock = () => {
 }
 choosePaper = () => {
     usersChoice = 'paper';
-    // console.log(usersChoice);
     startGame();
 }
 chooseScissors = () => {
@@ -235,18 +182,12 @@ chooseScissors = () => {
 
 startGame = () => {
     if (rounds > 0) {
-        // hide select round
         getComputersChoice();
         compareChoices();
     }
     else {
         outputInstruction.style.color = 'red';
     }
-
-}
-
-restartGame = () => {
-    // reload page
 }
 
 
